594e92f6ada72204f3983d5ca90b4930e0e0e8de
Sonyl Nagale <sonyl.nagale@nbcuni.com>
2017-12-27 11:23:45 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// add an event listener to the form to submit Dave's message
+
+// create a function for HAL to respond to Dave's messages with variable logic based upon
+// Dave's inputs
+
+// create a function for HAL to open the chat with "Good morning, Dave"
+
+// invoke the opening message
